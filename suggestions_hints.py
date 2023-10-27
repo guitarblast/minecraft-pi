ea2bdba70aca77c0ec42572c8094d05ab86e5b6b
Lukasz Przewlocki <ellockie@users.noreply.github.com>
2023-10-27 15:43:41 +0100

    Create suggestions_hints.py
    Kilka sugestii

---
+++ suggestions_hints.py
@@ -0,0 +1,12 @@
+# Prosty program:
+
+from mcpi import minecraft
+
+mc = minecraft.Minecraft.create()
+
+chat=raw_input("Napisz wiadomosc:")
+mc.postToChat(chat)
+
+
+# Przeczytajcie:
+- https://arghbox.wordpress.com/
